e636d7c54effa8b4efaf530c4e521615f6dd6788
Khanh Vu <kvu@nvidia.com>
2025-04-18 14:10:10 +0700

    update sqli

--- src/main/java/net/vinpos/api/config/SecurityConfig.java
+++ src/main/java/net/vinpos/api/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
     "/v3/api-docs",
     "/v3/api-docs/**",
     "/sqli/**",
+    "errors",
     "/stomp/**" // To allow the initial HTTP call to stomp handshake endpoint
   };
 
